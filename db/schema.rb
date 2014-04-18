527a31f0b9c4c9c8e4d88e51970a5a1f4ee0f3f5
Ross Fuhrman <rossfuhrman@gmail.com>
2014-04-18 11:39:35 -0500

    Add user ref to activities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140418161950) do
+ActiveRecord::Schema.define(version: 20140418163524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20140418161950) do
     t.string   "polarity"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "activities", ["user_id"], name: "index_activities_on_user_id", using: :btree
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
