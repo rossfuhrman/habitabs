2eb2d4582311143f0cc0ae83fb0ea085f8323b76
Ross Fuhrman <rossfuhrman@gmail.com>
2014-05-17 20:55:43 -0500

    Add helper to find marks for an activity on a given date

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,7 @@ class Activity < ActiveRecord::Base
   validates_presence_of :title, :user
   belongs_to :user
   has_many :marks , :dependent => :destroy
+  def marks_on_date date
+    marks.where(:mark_date => date).count
+  end
 end
