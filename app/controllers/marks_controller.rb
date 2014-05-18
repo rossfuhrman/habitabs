a1b0fb031cd9a2f703ae0ed92e9e2980d36de3a5
Ross Fuhrman <rossfuhrman@gmail.com>
2014-05-17 23:01:12 -0500

    Fade in the new count for the marks for the date of the activity when clicked

--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -4,7 +4,8 @@ class MarksController < ApplicationController
     respond_to do |format|
       if @mark.save
         format.html { redirect_to @mark, notice: 'Mark was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @mark }
+        #format.json { render action: 'show', status: :created, location: @mark }
+        format.json { render :json => @mark.activity.marks_on_date(mark_params[:mark_date]) }
       else
         format.html { render action: 'new' }
         format.json { render json: @mark.errors, status: :unprocessable_entity }
