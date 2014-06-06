f02174f3f788ef276e3002f1d307cf802ebcfae5
Ross Fuhrman <rossfuhrman@gmail.com>
2014-06-05 21:10:19 -0500

    Add time_zone setting to dev and test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Activities::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.time_zone = 'Central Time (US & Canada)'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
