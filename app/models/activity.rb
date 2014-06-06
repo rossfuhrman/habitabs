8eb2e37a161ad0cb9a3b3cbad9d9a8632a2bd75f
Ross Fuhrman <rossfuhrman@gmail.com>
2014-06-05 23:09:10 -0500

    Show an empty string instead of 0 for activities with no marks on a day

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,5 +4,9 @@ class Activity < ActiveRecord::Base
   has_many :marks , :dependent => :destroy
   def marks_on_date date
     marks.where(:mark_date => date).count
+    #this should probably not return anything if the count is zero
+    #if I make it return an empty string for 0,
+    #then the javascript doesn't work and the page isn't updated
+    #so I am doing that conversion in the js
   end
 end
