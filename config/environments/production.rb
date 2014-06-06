05ff1a890e4175c9f1bb8f06dc1ec06ac6bf0a19
Ross Fuhrman <rossfuhrman@gmail.com>
2014-06-05 20:55:32 -0500

    Setting default timezone to Cental Zone (US) This is not a final solution as it should be set per user or maybe even be location aware based on the user session, but this is good enough for now.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Activities::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  #
+  config.time_zone = 'Central Time (US & Canada)'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
