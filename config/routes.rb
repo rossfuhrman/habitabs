f196d77290ce470baef553c5e8ef99d9f25fee98
Ross Fuhrman <rossfuhrman@gmail.com>
2014-06-07 21:23:14 -0500

    After logging in, user should see the activities index

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Activities::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'activities#index'
 
   match '/marks/remove', to: 'marks#remove', via: [:post]
 
