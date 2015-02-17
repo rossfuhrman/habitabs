8b6c24c7a35d60336966840cefd9312019e85307
Ross Fuhrman <rossfuhrman@gmail.com>
2015-02-17 17:40:08 -0600

    Hook up email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Habits::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.deconfig.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com'
+    :user_name            => 'habitabsapp@gmail.com',
+    :password             => ENV['GMAIL_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
