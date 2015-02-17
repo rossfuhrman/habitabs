89d4aff58c88e87068000afc48f1adcd1a37f211
Ross Fuhrman <rossfuhrman@gmail.com>
2015-02-17 17:43:08 -0600

    Copy and paste error :( One more try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Habits::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.deconfig.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
